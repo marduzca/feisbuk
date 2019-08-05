e511db939d5b7420e41d75a440298094859fa018
marduzca <miguel.arduz@thoughtworks.com>
2019-08-05 17:59:31 +0200

    Add first classes

---
+++ src/main/User.kt
@@ -0,0 +1,7 @@
+package main
+
+import java.util.*
+
+class User(val username: String, val name: String, val country: String, val dayOfBirth: Date) {
+
+}
---
+++ src/test/FeisbukTest.kt
@@ -0,0 +1,11 @@
+package test
+
+import org.junit.Test
+
+
+internal class FeisbukTest {
+    @Test
+    fun test() {
+
+    }
+}
