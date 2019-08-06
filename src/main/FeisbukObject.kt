7a1ecfb47b884382d560da55c349ee41b0dbb212
marduzca <miguel.arduz@thoughtworks.com>
2019-08-06 17:10:10 +0200

    Small refactors

--- src/main/FeisbukObject.kt
+++ src/main/FeisbukObject.kt
@@ -1,7 +1,5 @@
 package main
 
-import java.time.LocalDate
-
 abstract class FeisbukObject(val idName: String, val name: String) {
     val wall: Wall = Wall()
 }
--- src/main/Person.kt
+++ src/main/Person.kt
@@ -2,6 +2,7 @@ package main
 
 import java.time.LocalDate
 
-class Person(idName: String, name: String, val country: String, val dayOfBirth: LocalDate): FeisbukObject(idName, name) {
+class Person(idName: String, name: String, val country: String, val dayOfBirth: LocalDate) :
+    FeisbukObject(idName, name) {
     val pagesLiked: ArrayList<Page> = ArrayList()
 }
