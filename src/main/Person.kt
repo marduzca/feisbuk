ea06732f846b7613a63886c5ab57965f9abc751a
marduzca <miguel.arduz@thoughtworks.com>
2019-08-06 16:06:04 +0200

    Change date type for day of birth

--- src/main/Person.kt
+++ src/main/Person.kt
@@ -1,5 +1,7 @@
 package main
 
-import java.util.*
+import java.time.LocalDate
 
-class Person(val username: String, val name: String, val country: String, val dayOfBirth: Date)
+class Person(val username: String, val name: String, val country: String, val dayOfBirth: LocalDate) {
+    val wall = Wall()
+}
